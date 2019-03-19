832855ab33491519aa8c3b092580c366647f50b4
Boris Shkarov <b.shkarov@gmail.com>
2019-03-19 17:01:42 +0300

    step 3+

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import readlineSync from 'readline-sync';
 
-export const welcome = () => {
+const welcome = () => {
   console.log('Welcome to the Brain Games!');
   const name = readlineSync.question('May I have your name? ');
   console.log('Hello, %s!', name);
 };
+
+export default welcome;
