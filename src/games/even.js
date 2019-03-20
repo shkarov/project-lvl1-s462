0b0860ca86174c0c995a25a5eb5b563ec58803d6
Boris Shkarov <b.shkarov@gmail.com>
2019-03-20 14:29:12 +0300

    correct step 4

--- src/games/even.js
+++ src/games/even.js
@@ -1,12 +1,14 @@
 import readlineSync from 'readline-sync';
 
-const COUNT_GAMES = 3;
-const INT_MIN = 2;
-const INT_MAX = 100;
+const countGames = 3;
+const intMin = 2;
+const intMax = 100;
 
 const getRandomInt = (min, max) => Math.floor(Math.random() * (max - min)) + min;
 
 const isEven = num => (num !== 0 && num % 2 === 0);
+
+const isRightAnswer = (num, answer) => (isEven(num) && answer === 'yes') || (!isEven(num) && answer === 'no');
 
 const checkEven = () => {
   console.log('Welcome to the Brain Games!');
@@ -14,23 +16,25 @@ const checkEven = () => {
   console.log(`Hello, ${player} !`);
   console.log('Answer "yes" if number even otherwise answer "no".');
 
-  const checkAnswer = (count, num) => {
+  const isCheckAnswer = (count, num) => {
     if (count < 1) {
-      console.log(`Congratulations, ${player} !`);
       return true;
     }
     console.log(`Question: ${num}`);
     const answer = readlineSync.question('Your answer:');
-    if ((isEven(num) && answer === 'yes') || (!isEven(num) && answer === 'no')) {
-      console.log('Correct!');
-      return checkAnswer(count - 1, getRandomInt(INT_MIN, INT_MAX));
+    if (!isRightAnswer(num, answer)) {
+      return false;
     }
-    console.log('"yes" is wrong answer ;(. Correct answer was "no".');
-    console.log(`Let's try again, ${player} !`);
-    return false;
+    console.log('Correct!');
+    return isCheckAnswer(count - 1, getRandomInt(intMin, intMax));
   };
 
-  return checkAnswer(COUNT_GAMES, getRandomInt(INT_MIN, INT_MAX));
+  if (isCheckAnswer(countGames, getRandomInt(intMin, intMax))) {
+    console.log(`Congratulations, ${player} !`);
+  } else {
+    console.log('"yes" is wrong answer ;(. Correct answer was "no".');
+    console.log(`Let's try again, ${player} !`);
+  }
 };
 
 export default checkEven;
