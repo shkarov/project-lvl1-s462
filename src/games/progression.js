1ec3a1828c864e06d74ee147e2ad9cb08eaee18c
Boris Shkarov <b.shkarov@gmail.com>
2019-03-22 16:46:53 +0300

    step 6/7

---
+++ src/games/progression.js
@@ -0,0 +1,50 @@
+import {
+  isPair,
+  cons,
+  car,
+  cdr,
+} from 'hexlet-pairs';
+import main from '../engine';
+
+const ruleGame = 'What number is missing in the progression ?';
+
+const getRandomInt = (min, max) => Math.round(min - 0.5 + Math.random() * (max - min + 1));
+
+const createProgression = (count, step, acc) => {
+  if (count === 0) {
+    return acc;
+  }
+  return createProgression(count - 1, step, cons(car(acc) + step, acc));
+};
+
+const createQuestion = (pair, acc, count, indexHidden) => {
+  if (!isPair(pair)) {
+    return acc;
+  }
+  const nextValue = (count === indexHidden) ? `.. ${acc}` : `${car(pair)} ${acc}`;
+  return createQuestion(cdr(pair), nextValue, count - 1, indexHidden);
+};
+
+const createAnswer = (pair, count, indexHidden) => {
+  if (count === indexHidden) {
+    return car(pair);
+  }
+  return createAnswer(cdr(pair), count - 1, indexHidden);
+};
+
+const createData = () => {
+  const numStart = getRandomInt(1, 100);
+  const numStep = getRandomInt(1, 20);
+  const indexHidden = getRandomInt(1, 10);
+
+  const progression = createProgression(9, numStep, cons(numStart, null));
+
+  const question = createQuestion(progression, '', 10, indexHidden);
+  const answer = createAnswer(progression, 10, indexHidden);
+
+  return cons(question, String(answer));
+};
+
+export default () => {
+  main(ruleGame, createData);
+};
